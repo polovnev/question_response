75eba66d0b0d4fe705d49b6babad93e4bf378dd5
oleksandr_polovnev <polovnevad@gmail.com>
2022-02-15 01:10:50 +0200

    git init

---
+++ src/main/java/com/polovnev/question_response/QuestionResponseApplication.java
@@ -0,0 +1,13 @@
+package com.polovnev.question_response;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionResponseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polovnev/question_response/QuestionResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polovnev.question_response;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionResponseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
