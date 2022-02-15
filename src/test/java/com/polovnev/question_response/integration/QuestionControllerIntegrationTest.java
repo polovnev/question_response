e21e3f27c6bc2b7ef6142bd824818b56dd7ab871
oleksandr_polovnev <polovnevad@gmail.com>
2022-02-15 07:00:13 +0200

    add test stub

---
+++ src/test/java/com/polovnev/question_response/integration/QuestionControllerIntegrationTest.java
@@ -0,0 +1,8 @@
+package com.polovnev.question_response.integration;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class QuestionControllerIntegrationTest {
+}
