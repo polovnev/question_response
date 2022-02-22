1d1e0740762e5701f1776e4dd1c9cfa47459aef7
oleksandr_polovnev <polovnevad@gmail.com>
2022-02-22 13:51:01 +0100

    create BaseTest for questions

---
+++ src/test/java/com/polovnev/question_response/springcloudcontractproducer/BaseTest.java
@@ -0,0 +1,52 @@
+package com.polovnev.question_response.springcloudcontractproducer;
+
+
+import com.polovnev.question_response.controller.QuestionController;
+import com.polovnev.question_response.dao.QuestionRepository;
+import com.polovnev.question_response.entity.Question;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@DirtiesContext
+@AutoConfigureMessageVerifier
+public class BaseTest {
+
+    @Autowired
+    private QuestionController questionController;
+
+    @MockBean
+    private QuestionRepository questionRepository;
+
+    @BeforeEach
+    public void setup() {
+        setupDbInteraction();
+        StandaloneMockMvcBuilder standaloneMockMvcBuilder
+                = MockMvcBuilders.standaloneSetup(questionController);
+        RestAssuredMockMvc.standaloneSetup(standaloneMockMvcBuilder);
+    }
+
+    private void setupDbInteraction() {
+        Question questionOne = Question.builder().id(1L).text("First question?")
+                .author(1L).location(1L).isResponded(true).createdDate(LocalDate.MIN).build();
+        Question questionTwo = Question.builder().id(2L).text("Second question?")
+                .author(1L).location(1L).isResponded(false).createdDate(LocalDate.MIN).build();
+        List<Question> questions = Arrays.asList(questionOne,questionTwo);
+        Mockito.when(questionRepository.findByLocation(1L)).thenReturn(questions);
+    }
+}
