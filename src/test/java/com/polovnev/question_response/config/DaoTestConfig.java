2ef48720929fa498ad40281b1f3232537e1dad63
oleksandr_polovnev <polovnevad@gmail.com>
2022-02-27 15:24:26 +0100

    DaoTestConfig

---
+++ src/test/java/com/polovnev/question_response/config/DaoTestConfig.java
@@ -0,0 +1,71 @@
+package com.polovnev.question_response.config;
+
+import com.polovnev.question_response.dao.QuestionRepository;
+import com.polovnev.question_response.dao.ResponseRepository;
+import com.polovnev.question_response.dao.TagRepository;
+import com.polovnev.question_response.entity.Question;
+import com.polovnev.question_response.entity.Response;
+import com.polovnev.question_response.entity.Tag;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@TestConfiguration
+public class DaoTestConfig {
+
+    @Primary
+    @Bean
+    public QuestionRepository questionRepositoryMock() {
+        QuestionRepository questionRepository = Mockito.mock(QuestionRepository.class);
+
+        Question questionOne = Question.builder().id(1L).text("First question?")
+                .author(1L).location(1L).isResponded(true).createdDate(LocalDate.MIN).build();
+        Question questionTwo = Question.builder().id(2L).text("Second question?")
+                .author(1L).location(1L).isResponded(false).createdDate(LocalDate.MIN).build();
+        List<Question> questions = Arrays.asList(questionOne, questionTwo);
+
+        Mockito.when(questionRepository.findByLocation(1L)).thenReturn(questions);
+        Mockito.when(questionRepository.findById(1L)).thenReturn(Optional.of(questionOne));
+
+        return questionRepository;
+    }
+
+    @Primary
+    @Bean
+    public ResponseRepository responseRepositoryMock() {
+        ResponseRepository responseRepository = Mockito.mock(ResponseRepository.class);
+
+        Response responseOne = Response.builder().id(1L).text("First response!")
+                .author(1L).isResponse(true).build();
+        Response responseTwo = Response.builder().id(2L).text("Second response!")
+                .author(1L).isResponse(true).build();
+        List<Response> responses = Arrays.asList(responseOne, responseTwo);
+
+        Mockito.when(responseRepository.findByQuestion_Id(1L)).thenReturn(responses);
+
+        return responseRepository;
+    }
+
+
+
+    @Primary
+    @Bean
+    public TagRepository tagRepositoryMock() {
+        TagRepository tagRepository = Mockito.mock(TagRepository.class);
+
+        Tag tagOne = Tag.builder().id(1L).text("Документы").color("#FFF8DC").build();
+        Tag tagTwo = Tag.builder().id(2L).text("Еда").color("#7FFF00").build();
+
+        List<Tag> tags = Arrays.asList(tagOne, tagTwo);
+
+        Mockito.when(tagRepository.findAll()).thenReturn(tags);
+
+        return tagRepository;
+    }
+}
--- src/test/java/com/polovnev/question_response/springcloudcontractproducer/BaseTest.java
+++ src/test/java/com/polovnev/question_response/springcloudcontractproducer/BaseTest.java
@@ -1,37 +1,24 @@
 package com.polovnev.question_response.springcloudcontractproducer;
 
 
+import com.polovnev.question_response.config.DaoTestConfig;
 import com.polovnev.question_response.controller.QuestionController;
 import com.polovnev.question_response.controller.ResponseController;
 import com.polovnev.question_response.controller.TagController;
-import com.polovnev.question_response.dao.QuestionRepository;
-import com.polovnev.question_response.dao.ResponseRepository;
-import com.polovnev.question_response.dao.TagRepository;
-import com.polovnev.question_response.entity.Question;
-import com.polovnev.question_response.entity.Response;
-import com.polovnev.question_response.entity.Tag;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
+@Import(DaoTestConfig.class)
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@DirtiesContext
-@AutoConfigureMessageVerifier
 public abstract class BaseTest {
 
     @Autowired
@@ -43,56 +30,13 @@ public abstract class BaseTest {
     @Autowired
     private TagController tagController;
 
-    @MockBean
-    private QuestionRepository questionRepository;
-
-    @MockBean
-    private ResponseRepository responseRepository;
-
-    @MockBean
-    private TagRepository tagRepository;
 
     @BeforeEach
     public void setup() {
-        setupDbInteraction();
         StandaloneMockMvcBuilder standaloneMockMvcBuilder
-                = MockMvcBuilders.standaloneSetup(questionController, responseController, tagController);
+                = MockMvcBuilders.standaloneSetup(questionController,
+                responseController, tagController);
         RestAssuredMockMvc.standaloneSetup(standaloneMockMvcBuilder);
     }
 
-    private void setupDbInteraction() {
-        setupDbInteractionForQuestions();
-        setupDbInteractionForResponses();
-        setupDbInteractionForTags();
-    }
-
-    private void setupDbInteractionForQuestions() {
-        Question questionOne = Question.builder().id(1L).text("First question?")
-                .author(1L).location(1L).isResponded(true).createdDate(LocalDate.MIN).build();
-        Question questionTwo = Question.builder().id(2L).text("Second question?")
-                .author(1L).location(1L).isResponded(false).createdDate(LocalDate.MIN).build();
-        List<Question> questions = Arrays.asList(questionOne, questionTwo);
-
-        Mockito.when(questionRepository.findByLocation(1L)).thenReturn(questions);
-        Mockito.when(questionRepository.findById(1L)).thenReturn(Optional.of(questionOne));
-    }
-
-    private void setupDbInteractionForResponses() {
-        Response responseOne = Response.builder().id(1L).text("First response!")
-                .author(1L).isResponse(true).build();
-        Response responseTwo = Response.builder().id(2L).text("Second response!")
-                .author(1L).isResponse(true).build();
-        List<Response> responses = Arrays.asList(responseOne, responseTwo);
-
-        Mockito.when(responseRepository.findByQuestion_Id(1L)).thenReturn(responses);
-    }
-
-    private void setupDbInteractionForTags() {
-        Tag tagOne = Tag.builder().id(1L).text("Документы").color("#FFF8DC").build();
-        Tag tagTwo = Tag.builder().id(2L).text("Еда").color("#7FFF00").build();
-
-        List<Tag> tags = Arrays.asList(tagOne, tagTwo);
-
-        Mockito.when(tagRepository.findAll()).thenReturn(tags);
-    }
 }
